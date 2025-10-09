57ff9e32a95f6d80aba4994549ef6af2937de61e
Mahdiyar Ashrafioun <mahdiyar.ashrafioun@ucalgary.ca>
2025-10-08 18:15:06 -0600

    haskell file added and initialized

---
+++ A2.hs
@@ -0,0 +1,21 @@
+-- Name: Mahdiyar Ashrafioun
+-- UCID: 30232243 
+-- Tut: 02
+-- Date: 10/8/25
+-- Assignment 2 
+
+module A2 where
+
+iteratedCartesianProduct :: [a] -> Int -> [[a]]
+iteratedCartesianProduct =  
+
+
+numSums :: Int -> Int
+numSums = 
+
+ballsIntoBins :: Int -> Int -> Int
+ballsIntoBins = 
+
+
+iRunLessArrs :: Int -> Int -> Int
+iRunLessArrs =
