2198637bae0e9fac0078015562679909d7cba686
Mahdiyar Ashrafioun <mahdiyar.ashrafioun@ucalgary.ca>
2025-10-12 21:32:10 -0600

    final version of code added

--- A2.hs
+++ A2.hs
@@ -6,23 +6,55 @@
 
 module A2 where
 
+-- p1
 iteratedCartesianProduct :: [a] -> Int -> [[a]]
 iteratedCartesianProduct _ 0 = [[]]
 iteratedCartesianProduct l n = [x:xs | x <- l, xs <- iteratedCartesianProduct l (n-1)]
 
-
+--p2
 numSums :: Int -> Int
 numSums n = length [xs | k <- [1..n],
                          xs <- iteratedCartesianProduct [1..n] k,
                          sum xs == n,
                          and [x <= y | (x,y) <- zip xs (tail xs)]]
                          
-
+--p3
 ballsIntoBins :: Int -> Int -> Int
 ballsIntoBins k n = length [xs | xs <- iteratedCartesianProduct [1..n] k,
                                  and [i `elem` xs | i <- [1..n]]]
  
+--p4
+-- a
+hasConsecElem :: [Int] -> Bool
+hasConsecElem xs = any (\(a,b) -> abs (a - b) == 1) (zip xs (tail xs ++ [head xs]))
 
+-- b
+twoRunLessArrs :: Int -> Int
+twoRunLessArrs n =
+  length [arr | arr <- allArrs [1..n],
+                not (hasConsecElem arr),
+                head arr == minimum arr]
+  where
+    allArrs [] = [[]]
+    allArrs xs = [y:zs | y <- xs, zs <- allArrs [x | x <- xs, x /= y]]
 
+--c
+hasConsecSubl :: [Int] -> Int -> Bool
+hasConsecSubl xs i =
+  any runAt [0..(n-1)]
+  where
+    n = length xs
+    ys = xs ++ take (i-1) xs
+    runAt j =
+      all (\k -> ys !! (j+k+1) - ys !! (j+k) == 1) [0..i-2] ||
+      all (\k -> ys !! (j+k) - ys !! (j+k+1) == 1) [0..i-2]
+
+-- d
 iRunLessArrs :: Int -> Int -> Int
-iRunLessArrs =
+iRunLessArrs n i =
+  length [arr | arr <- allArrs [1..n],
+                not (hasConsecSubl arr i),
+                head arr == minimum arr]
+  where
+    allArrs [] = [[]]
+    allArrs xs = [y:zs | y <- xs, zs <- allArrs [x | x <- xs, x /= y]]
